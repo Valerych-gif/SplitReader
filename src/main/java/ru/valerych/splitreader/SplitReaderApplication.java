c31b66ba212e963b994eba1136c1e45d95e6806a
Valery Bulekov <muzaccord@gmail.com>
2020-12-30 10:17:22 +0300

    Initial commit

---
+++ src/main/java/ru/valerych/splitreader/SplitReaderApplication.java
@@ -0,0 +1,13 @@
+package ru.valerych.splitreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/valerych/splitreader/SplitReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.valerych.splitreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
